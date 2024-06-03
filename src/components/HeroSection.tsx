66151ade395e67709cd6b3e8036e0c15c4f3371c
Alok Yadav <yalok6321@gmail.com>
2024-06-03 20:46:52 +0530

    Featured section Done

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -18,7 +18,7 @@ const HeroSection = () => {
           journey today. Whether you&apos;re a beginner or looking to refine your
           skills, join us to unlock your true potential
         </p>
-        <div className="mt-4">
+        <div className="mt-10">
           <Link href={"/explore-courses"}>
             <Button
               borderRadius="1.75rem"
