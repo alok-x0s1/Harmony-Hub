6a5e85cc70ac60455771664723d7f86cf2941192
Alok Yadav <yalok6321@gmail.com>
2024-06-07 14:04:26 +0530

    Instructor section DONE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructor from "@/components/Instructor";
 import MovingCards from "@/components/MovingCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -12,6 +13,7 @@ export default function Home() {
       <WhyChooseUs />
       <MovingCards />
       <UpcomingWebinars />
+      <Instructor />
     </main>
   );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/moving-border";
 
 const HeroSection = () => {
   return (
-    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+    <div className="h-auto md:h-[50rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
